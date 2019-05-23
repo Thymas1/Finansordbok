089f2cac0e5c8212a76441d2f23c0748c1e87c9f
Thomas Endresen <thomasjohanendresen@gmail.com>
2019-05-23 13:17:16 +0200

    add(Styledcomponents): added styled-components functionality

---
+++ web/src/components/Title.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import React from "react"
+
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-allign: center;
+  color: palevioletred;
+  `;
+  const Wrapper = styled.section`
+  padding:4em;
+  background: papayawhip;
+  `;
+
+export default Title;
